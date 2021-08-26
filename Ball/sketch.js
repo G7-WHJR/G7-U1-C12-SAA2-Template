3e82d39ff4aa50b9787456a06a99e4b4f3077ecb
unknown <raveen.anand@whitehatjr.com>
2021-08-26 20:37:45 +0530

    First

---
+++ Ball/sketch.js
@@ -0,0 +1,22 @@
+var white,orange;
+
+function setup() {
+  //size of the canvas to draw on
+  createCanvas(innerWidth, innerHeight);
+
+  // Set the background to white, and move the line to setup();
+  background("white");
+
+}
+
+
+function draw() {
+  
+  white = new Ball(150, 50, 25, "white");
+  white.appear();
+  orange = new Ball(150, 100, 50, "orange");
+  orange.appear();
+    
+}
+    
+
